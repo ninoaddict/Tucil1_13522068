2c87e105c36ea5c6b327f3982b1f0d6ce6f5ecd7
ninoaddict <adrilbless37@gmail.com>
2024-02-09 15:48:10 +0700

    feat: add bruteforce function

---
+++ src/breach-protocol/app/lib/bruteforce.ts
@@ -0,0 +1,97 @@
+import { getIndex, getPoint } from "./util";
+
+export default function runBruteForce(data: {
+  bufferSize: number;
+  matrix: string[][];
+  sequences: string[];
+  rewards: number[];
+}) {
+  // variable declaration
+  const { bufferSize, matrix, sequences, rewards } = data;
+  const vis = Array.from(Array(matrix.length), () =>
+    Array(matrix[0].length).fill(false)
+  );
+  let maxPoint = 0;
+  let maxCoordinate: number[] = [];
+
+  // function declaration
+  function searchOptimalValue(
+    row: number,
+    col: number,
+    coordinates: number[],
+    direction: boolean,
+    token: string,
+    steps: number
+  ) {
+    // init
+    coordinates.push(getIndex(row, col, matrix[0].length));
+    vis[row][col] = true;
+
+    // check current point
+    let currPoint = getPoint(token, sequences, rewards);
+    if (currPoint > maxPoint) {
+      maxPoint = currPoint;
+      maxCoordinate = [...coordinates];
+    }
+
+    // check if steps is equal to buffer size
+    if (steps < bufferSize) {
+      if (direction) {
+        for (let i = 0; i < matrix.length; i++) {
+          if (!vis[i][col]) {
+            searchOptimalValue(
+              i,
+              col,
+              coordinates,
+              !direction,
+              token + matrix[i][col],
+              steps + 1
+            );
+          }
+        }
+      } else {
+        for (let i = 0; i < matrix[0].length; i++) {
+          if (!vis[row][i]) {
+            searchOptimalValue(
+              row,
+              i,
+              coordinates,
+              !direction,
+              token + matrix[row][i],
+              steps + 1
+            );
+          }
+        }
+      }
+    }
+
+    // termination
+    coordinates.pop();
+    vis[row][col] = false;
+    return;
+  }
+
+  const startTime = performance.now();
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    searchOptimalValue(0, i, [], true, matrix[0][i], 1);
+  }
+
+  const endTime = performance.now();
+  const runTime = endTime - startTime;
+
+  return { maxPoint, maxCoordinate, runTime };
+}
+
+// function searchOptimalValue(
+//   row: number,
+//   col: number,
+//   coordinates: number[],
+//   direction: boolean,
+//   token: string,
+//   steps: number,
+//   bufferSize: number,
+//   matrix: string[][],
+//   sequences: string[],
+//   rewards: number[]
+// ) {}
